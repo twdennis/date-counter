0f3c06c8a71900292272eb22fa5498542eb80aee
Tom <131278560+twdennis@users.noreply.github.com>
2024-01-16 11:30:26 +0000

    refactored, fixed some styling issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  return <DateCounter />;
+}
+
+function DateCounter() {
+  const [interval, setInterval] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box">
+        <div className="counter-div">
+          <button className="btn" onClick={() => setInterval((i) => i - 1)}>
+            ➖
+          </button>
+          <div className="counter-total">Step: {interval}</div>
+          <button className="btn" onClick={() => setInterval((i) => i + 1)}>
+            ➕
+          </button>
+        </div>
+        <div className="counter-div">
+          <button className="btn" onClick={() => setCount((c) => c - interval)}>
+            ➖
+          </button>
+          <div className="counter-total">Count: {count}</div>
+          <button className="btn" onClick={() => setCount((c) => c + interval)}>
+            ➕
+          </button>
+        </div>
+        <div>
+          {count === 0
+            ? `Today's date is ${date.toDateString()}`
+            : count === 1
+            ? `Tomorrow is ${date.toDateString()}`
+            : count === -1
+            ? `Yesterday was ${date.toDateString()}`
+            : count < -1
+            ? `${count * -1} days ago was ${date.toDateString()}`
+            : `${count} days from today is ${date.toDateString()}`}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
